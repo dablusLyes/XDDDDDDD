4c3449ed8d9968eb4606c18e7332e72eb38b1bd6
Lyes <lyes.dabla@gmail.com>
2024-07-04 12:36:35 +0200

    lll

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -19,18 +17,24 @@ function App() {
     "#FF33A8"
   ]
 
+  const getRandomColor = () => {
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    return colors[randomIndex];
+  };
+
   const addToList = () => {
     addTodo([...todoList, text])
+    setText('')
   }
   return (
     <>
+      <h1>Today</h1>
+      <input onChange={(e) => { setText(e.target.value) }} onKeyDown={(e) => { if (e.key === 'Enter') addToList() }} value={text} className='todoInput' type="text" />
       {todoList.map((el, id) => {
         return (
-          <p key={id} className='todoElement' styles={{ 'background-color': colors[1] }} >{el}</p>
+          <p key={id} className='todoElement' style={{ background: getRandomColor() }} >{el}</p>
         )
       })}
-      <input onChange={(e) => { setText(e.target.value) }} className='todoInput' type="text" />
-      <button onClick={addToList} >add</button>
     </>
   )
 }
