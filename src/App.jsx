7870018154202ec4c239953e9fa5671ffe5f1cf4
Lyes Ramdane <lyes.dabla@gmail.com>
2024-07-06 22:28:43 +0200

    XDDDDDDDDDDDDDDDDDDDDDDDDDDDD

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
 import { useState, useRef, useEffect } from 'react'
 import Todo from './components/todo';
 import './App.css'
-import Draggable, { DraggableCore } from "react-draggable";
 function App() {
-  const todoElement = useRef();
-  const [todoList, setTodoList] = useState([{ text: 'zebi', isDone: false }])
+  const [todoList, setTodoList] = useState([{ text: '0', isDone: false }, { text: '1', isDone: false }, { text: '2', isDone: false }, { text: '3', isDone: false },])
   const [text, setText] = useState('')
-  const [isDragged, setDragged] = useState(false)
-  const [xOffset, setXOffset] = useState(0);
-  const [enterPos, setEnterPos] = useState(0)
-  const [position, setPosition] = useState({ x: 0, y: 0 });
+
 
 
 
@@ -21,45 +16,19 @@ function App() {
     }
   }
 
-  function handleDrag(e, id) {
-    console.log('zebiiii');
-    if (isDragged) {
-      if ((e.clientX - enterPos) > 30) {
-        let changedEl = { text: todoList[id].text, isDone: true }
-        setTodoList(todoList.splice(id, 1, changedEl))
-      }
-    }
-  }
-  const onMouseDown = (e) => { setDragged(true), setEnterPos(e.clientX), setXOffset(0) }
-  const onMouseUp = () => { setDragged(false), setXOffset(0) }
-  const onMouseOut = () => { setDragged(false), setXOffset(0) }
   return (
-    <>
+    <div>
       <div className="head">
         <p className="title">🫵 Today</p>
       </div>
-      <input onChange={(e) => { setText(e.target.value) }} placeholder='enter todo' onKeyDown={(e) => { if (e.key === 'Enter') addToList() }} value={text} className='todoInput' type="text" />
+      <input onChange={(e) => { setText(e.target.value) }} placeholder='+' onKeyDown={(e) => { if (e.key === 'Enter') addToList() }} value={text} className='todoInput' type="text" />
       {todoList.map((el, id) => {
         let idx = id
         return (
-          <Draggable
-            ref={todoElement}
-
-            position={position}
-            axis='x'
-            onDrag={(e) => {
-              handleDrag(e, idx)
-            }}
-            onMouseDown={onMouseDown}
-            onMouseUp={onMouseUp}
-            key={id}
-          >
-            <Todo id={idx} text={el.text} isDone={el.isDone}></Todo>
-          </Draggable>
-
+          <Todo key={id} todoList={todoList} setTodoList={setTodoList} id={idx} text={el.text} isDone={el.isDone}></Todo>
         )
       })}
-    </>
+    </div>
   )
 }
 
--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -1,29 +1,86 @@
-import React from "react";
-const colors = [
-	"#FF5733",
-	"#FFBD33",
-	"#75FF33",
-	"#33FF57",
-	"#33FFBD",
-	"#33CFFF",
-	"#3357FF",
-	"#7533FF",
-	"#BD33FF",
-	"#FF33A8",
-];
-function Todo({ id, text, isDone }) {
+import { React, useEffect, useRef, useState } from "react";
+import Draggable, { DraggableCore } from "react-draggable";
+import colors from "../../colors";
+
+
+
+
+
+function Todo({ todoList, setTodoList, id, text, isDone }) {
+	const todoElement = useRef();
+	const [isDragged, setDragged] = useState(false)
+	const [xOffset, setXOffset] = useState(0);
+	const [enterPos, setEnterPos] = useState(0)
+	const [position, setPosition] = useState({ x: 0, y: 0 });
+	const [DragState, setDragState] = useState(null)
+	const [DraggedElement, setDraggedElement] = useState(null)
+	let offsetAction = 200
+
+	function handleDrag(e, id) {
+
+		if (isDragged) {
+			if ((e.clientX - enterPos) > offsetAction) {
+				if (!todoList[id].isDone) setDragState('checkTodo')
+			} else if ((e.clientX - enterPos) < -offsetAction) {
+				setDragState('delete')
+			}
+		}
+	}
+
+	const onMouseDown = (e) => { setDragged(true), setEnterPos(e.clientX), setXOffset(0) }
+	const onMouseUp = (id) => {
+		if (isDragged) {
+			if (DragState == 'checkTodo') {
+				console.log('Checking id: ', id);
+				elementDone(id)
+			} else if (DragState == 'delete') {
+				deleteElement(id)
+			}
+			setDragged(false)
+			setEnterPos(0)
+			setDragState(null)
+		}
+	}
+	const onMouseOut = () => { setDragged(false), setEnterPos(0) }
+
+	function deleteElement(id) {
+		let newTodoArr = [...todoList]
+		newTodoArr.splice(id, 1)
+		setTodoList(newTodoArr)
+		setDragged(false)
+	}
+
+	function elementDone(id) {
+		let changedEl = { text: todoList[id].text, isDone: true }
+		let newTodoArr = [...todoList]
+		newTodoArr.splice(id, 1)
+		newTodoArr.push(changedEl)
+		setTodoList(newTodoArr)
+		setDragged(false)
+	}
+	let idx = id
 	return (
 		<>
-			<div
-				key={id}
-				id={`todoElement${id}`}
-				className={isDone ? "todoDone todoElement " : "todoElement "}
-				style={{
-					background: colors[id],
+			<Draggable
+
+				position={position}
+				axis='x'
+				onStop={(id) => { onMouseUp(idx) }}
+				onDrag={(e) => {
+					handleDrag(e, idx)
 				}}
+				onStart={(e) => { onMouseDown(e), setDragged(true) }}
 			>
-				<p key={id}>{text}</p>
-			</div>
+				<div
+					className={isDone ? "todoDone todoElement " : "todoElement "}
+					id={`todoElement${id}`}
+					style={isDone ? {
+						background: 'rgb(57, 187, 96)',
+					} : { background: colors[idx] }}
+				>
+					<p >{text}</p>
+				</div>
+			</Draggable>
 		</>
 	);
 }
